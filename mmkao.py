b91cd7fe226769852b2df50898f679f74a3a4f8e
Tanky <wtq2255@icloud.com>
2015-09-05 03:28:49 +0800

    对 mmkao.py 添加多线程处理

--- mmkao.py
+++ mmkao.py
@@ -1,8 +1,15 @@
 # coding:utf8
 
 import urllib, urllib2
+import requests
 from bs4 import BeautifulSoup
-import os, sys
+from multiprocessing.dummy import Pool
+import os, sys, time
+from pprint import pprint
+
+TIMEOUT = 60
+
+imageCount = 0
 
 pwd = sys.path[0] + os.sep + "img" + os.sep # 设置图片存放路径为 当前文件夹下的img文件夹
 if os.path.exists(pwd) == False: # 若img文件夹不存在 新建文件夹img
@@ -12,11 +19,12 @@ if os.path.exists(pwd) == False: # 若img文件夹不存在 新建文件夹img
 def readPage(url):
     soup = None
     try:
-        print u"请求网址为:" + url
-        response = urllib2.urlopen(url)
-        soup = BeautifulSoup(response.read())
+        print u"请求网址为: %s" % url
+        response = requests.get(url, timeout = TIMEOUT)
+        response.encoding = 'gb2312'
+        soup = BeautifulSoup(response.text, 'html.parser')
     except Exception, e:
-        raise e
+        print e
     return soup
 
 def analyzeMainList(soup, url):
@@ -25,9 +33,6 @@ def analyzeMainList(soup, url):
         mainList.append(url + a.attrs["href"])
     # print mainList
     return mainList
-
-
-
 
 def analyzeImagePageList(soup, url):
     imageListPage = list()
@@ -40,22 +45,24 @@ def analyzeImagePageList(soup, url):
 def analyzeImageList(soup):
     imageList = list()
     for image in soup.select(".file > img"):
-        imageList.append(image.attrs["src"])
+        imageList.append(image.attrs["src"].split('\r')[0])
     return imageList
 
-
 def downloadImage(url):
-    # print url
     localPath = pwd + url.split("/")[4] + "-" + url.split("/")[5] + "-" + url.split("/")[6].replace("\r","")
+    print u'%s 开始下载!' % url
     try:
-        print url + u'开始下载!'
-        filename, msg = urllib.urlretrieve(url, localPath.replace("\r",""), reporthook = reporthook)
+        r = requests.get(url, timeout = TIMEOUT)
+        f = open(localPath, 'wb')
+        f.write(r.content)
     except Exception, e:
-        raise e
+        print e
+    else:
+        global imageCount
+        imageCount += 1
+        print u'%s 下载完成!' % localPath
     finally:
-        urllib.urlcleanup()
-        print localPath + u'下载完成!'
-
+        f.close()
 
 def reporthook(blocks_read, block_size, total_size):
     if not blocks_read:
@@ -68,38 +75,53 @@ def reporthook(blocks_read, block_size, total_size):
         print 'Read %d blocks, or %d/%d' % (blocks_read, amount_read, total_size)
     return
 
+def getMainAllList(i, url):
+    if i == 1:
+        mainListSoup = readPage(url)
+    else:
+        mainListSoup = readPage('%s%s.html' % (url, i))
+    return analyzeMainList(mainListSoup, url)
+
+def getImageAllList(main, mainAllList):
+    imageListSoup = readPage(main)
+    imagePageList = analyzeImagePageList(imageListSoup, main)
+    imageAllList = list()
+    for imagePage in imagePageList:
+        if imagePage == main:
+            imageList = analyzeImageList(imageListSoup)
+        else:
+            imageListSoup = readPage(imagePage)
+            imageList = analyzeImageList(imageListSoup)
+        imageAllList.extend(imageList)
+    time.sleep(2)
+    return imageAllList
+
+def main():
+    url = 'http://www.mmkao.com/ROSI/'
+    maxPage = 1
+    pool = Pool(processes=10)
+    mainAllList = []
+    for x in [pool.apply_async(getMainAllList, (i,url)) for i in range(1,maxPage + 1)]:
+        mainAllList.append(x.get())
+    pool.close()
+    pool.join()
+    print 'mainAllList', len(mainAllList)
+    pool = Pool(processes=4)
+    imageAllList = []
+    for x in [pool.apply_async(getImageAllList, (main, mainAllList[0])) for main in mainAllList[0]]:
+        imageAllList.extend(x.get())
+    print 'imageAllList', len(imageAllList)
+    pool.close()
+    pool.join()
+    pool = Pool(processes=4)
+    for img in sorted(imageAllList):
+        pool.apply_async(downloadImage, (img,))
+    pool.close()
+    pool.join()
+    print '共下载%s张图片' % imageCount
+
+        
+
 
 if __name__ == "__main__":
-    url = 'http://www.mmkao.com/ROSI/'
-    maxPage = 33
-    mainAllList = list()
-    for i in range(1,maxPage + 1):
-        if i == 1:
-            mainListSoup = readPage(url)
-        else:
-            mainListSoup = readPage(url + str(i) + '.html')
-        mainList = analyzeMainList(mainListSoup, url)
-        mainAllList.extend(mainList)
-    n = 0
-    for main in mainAllList:
-        n = n + 1
-        imageListSoup = readPage(main)
-        imagePageList = analyzeImagePageList(imageListSoup, main)
-        imageAllList = list()
-        for imagePage in imagePageList:
-            if imagePage == main:
-                imageList = analyzeImageList(imageListSoup)
-            else:
-                imageListSoup = readPage(imagePage)
-                imageList = analyzeImageList(imageListSoup)
-            imageAllList.extend(imageList)
-        m = 0
-        for image in imageAllList:
-            m = m + 1
-            downloadImage(image)
-        print u"第" + str(n) + u"组已下完,还剩" + str(len(mainAllList) - m) + u"组图片"
-        print u"="*40
-        print u"="*40
-
-
-
+    main()
